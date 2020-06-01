21ced112fd49ce8274d3fd194e1d0ff0fa056ab2
Fabian Lindarte <Fabian Lindarte@DESKTOP-DTG4H7V>
2020-06-01 10:54:27 -0500

    Version Fabian Lindarte 10:54

--- src/cv_ejercicio/Cv_ejercicio.java
+++ src/cv_ejercicio/Cv_ejercicio.java
@@ -16,7 +16,8 @@ public class Cv_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("Version 0");
-        System.out.println("Version 1");       
+        System.out.println("Version 1");
+        System.out.println("Version Fabian Lindarte");
     }
     
 }
